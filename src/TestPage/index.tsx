57c3ef3a66c50bf98ae4e8601f851f2c63760d87
igorpichnenko <igorpichnenko@gmail.com>
2022-11-14 23:31:41 +0600

    fix adaptive

--- src/TestPage/index.tsx
+++ src/TestPage/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Calendar } from '../components/Calendar';
 import { eventsData } from '../mocks';
-import styles from "./index.module.scss"
 
 function TestPage() {
 
   return (
-    <section className={styles.testPage}>
-      <Calendar eventsData={eventsData} />
-    </section>
+    <Calendar eventsData={eventsData} />
   )
 }
 
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -34,34 +34,37 @@ export const Calendar: React.FC<CalendarProps> = ({ eventsData }) => {
     return (
         <section className={styles.wrapper}>
             <div className={styles.calendar}>
-                <h2 className={styles.title}>Календарь</h2>
+                <div className={styles.header}>
+                    <h2 className={styles.title}>Календарь</h2>
 
-                <div>
-                    <button className={styles.pagingButton} onClick={prevMonth}> <i className={styles.icon}></i></button>
-                    <button className={styles.pagingButton} onClick={nextMonth}> <i className={styles.icon}></i></button>
+                    <div>
+                        <button className={styles.pagingButton} onClick={prevMonth}><i className={styles.icon}></i></button>
+                        <button className={styles.pagingButton} onClick={nextMonth}><i className={styles.icon}></i></button>
+                    </div>
+
+                    <table>
+                        <caption className={styles.caption}>{calendarState.currentDate.format('MMMM YYYY')}</caption>
+                        <tbody>
+                            <tr className={styles.calendarBody}>
+                                {daysOfTheWeek.map((el) => (
+                                    <th className={styles.dayHeader} key={el}>{el}</th>
+                                ))}
+
+                                {calendarDays && calendarDays.map(({ date, opacity, color, value, backgroundColor, past }) => (
+                                    <td
+                                        key={date}
+                                        className={styles.day} onClick={() => handleClickCell(calendarDays, date)}>
+                                        <span className={styles.dayNumber} style={{ backgroundColor, opacity, color }}>{value}</span>
+                                        <Event past={past} handleClickEvent={handleClickEvent} date={date} calendarState={calendarState} />
+                                    </td>
+                                ))}
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-
-                <table>
-                    <caption className={styles.caption}>{calendarState.currentDate.format('MMMM YYYY')}</caption>
-                    <tbody>
-                        <tr className={styles.calendarBody}>
-                            {daysOfTheWeek.map((el) => (
-                                <th className={styles.dayHeader} key={el}>{el}</th>
-                            ))}
-
-                            {calendarDays && calendarDays.map(({ date, opacity, color, value, backgroundColor, past }) => (
-                                <td
-                                    key={date}
-                                    className={styles.day} onClick={() => handleClickCell(calendarDays, date)}>
-                                    <span className={styles.dayNumber} style={{ backgroundColor, opacity, color }}>{value}</span>
-                                    <Event past={past} handleClickEvent={handleClickEvent} date={date} calendarState={calendarState} />
-                                </td>
-                            ))}
-                        </tr>
-                    </tbody>
-                </table>
+                <EventForm onSubmit={handleSubmit} currentCheckedDate={currentCheckedDate} />
             </div>
-            <EventForm onSubmit={handleSubmit} currentCheckedDate={currentCheckedDate} />
+
             <EventPopup isEventPopup={isEventPopup} handleDeleteEvent={handleDeleteEvent} setEventPopup={setEventPopup} currentEvent={currentEvent} />
         </section>
     );
